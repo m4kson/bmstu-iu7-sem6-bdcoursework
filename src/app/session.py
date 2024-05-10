d27f8d288725806cc5d9199a126fcdcbee746b8d
m4ks0n <limos.00@mail.ru>
2024-05-10 18:35:56 +0300

    backend started

---
+++ src/app/session.py
@@ -0,0 +1,20 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text, URL
+from config import settings
+
+def create_connection(recreate=False):
+    print("Версия SQL Alchemy:", sqlalchemy.__version__)
+
+    engine = create_engine(
+        url=settings.DATABASE_URL_psycopg,
+        echo=True,
+    )
+
+    with engine.connect() as conn:
+        res = conn.execute(text("SELECT VERSION()"))
+        print(f"{res=}")
+
+
+if __name__ == "__main__":
+    create_connection(True)
+
