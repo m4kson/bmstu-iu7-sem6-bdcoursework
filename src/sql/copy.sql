6d4fe974c6457eca932c11f174bc6ffed263c083
m4kson <limos.00@mail.ru>
2024-05-15 12:55:55 +0300

    generation test data for tractors, lines, details and users added

--- src/sql/copy.sql
+++ src/sql/copy.sql
@@ -1,49 +1,19 @@
 COPY tractors 
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/actors.csv'
+FROM '/Users/m4ks0n/study/IU7/sem6/dbcourse/bmstu-iu7-sem6-bdcoursework/doc/testdata/tractors.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY assemblylines
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/companies.csv'
+FROM '/Users/m4ks0n/study/IU7/sem6/dbcourse/bmstu-iu7-sem6-bdcoursework/doc/testdata/assemblylines.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY details
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/directors.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY detailorders
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/films.csv'
+FROM '/Users/m4ks0n/study/IU7/sem6/dbcourse/bmstu-iu7-sem6-bdcoursework/doc/testdata/details.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY users
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/played.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY servicerequests
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/played.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY servicereports
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/played.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY order_detail
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/played.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY tractor_line
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/played.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY line_detail
-FROM '/Users/m4ks0n/study/IU7/sem5/bmstu-sem5-db/lab_01/tables/played.csv'
+FROM '/Users/m4ks0n/study/IU7/sem6/dbcourse/bmstu-iu7-sem6-bdcoursework/doc/testdata/users.csv'
 DELIMITER ','
 CSV HEADER;
