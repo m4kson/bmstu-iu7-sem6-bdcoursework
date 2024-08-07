1f75b39e757b6f0146e19e2b6f74d5203dd9d4c9
m4kson <limos.00@mail.ru>
2024-08-07 15:19:49 +0300

    #17 performance test added

---
+++ src/app/performance_tests/query_benchmark.py
@@ -0,0 +1,56 @@
+import asyncio
+import datetime
+import time
+import timeit
+from typing import Annotated
+from fastapi import Depends
+from app.models.models import ServiceRequest
+from app.repository.repository import ServiceRequestRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.filters import ServiceRequestsFilter
+from app.schemas.schemas import SServiceRequestWrite
+from app.session import create_session, get_db, async_session
+
+
+measurements_number = 100
+
+async def query_to_measure_1(repo: ServiceRequestRepository, filter: ServiceRequestsFilter):
+    return await repo.get_all_service_requests(filter)
+
+async def query_to_measure_2(repo: ServiceRequestRepository, request: SServiceRequestWrite, user_id: int):
+    service_request = SServiceRequestWrite(
+            lineid=1,
+            type="ремонт",
+            description="замер времени"
+        )
+    await repo.add_service_request(service_request, 1)
+    
+
+async def measure_query(query_func, *args, num_runs: int):
+    total_time = 0
+    for _ in range(num_runs):
+        start_time = time.perf_counter()
+        await query_func(*args)
+        total_time += time.perf_counter() - start_time
+
+    average_time = total_time / num_runs
+    print(f"Среднее время выполнения запроса: {average_time:.6f} секунд")
+
+async def run_measurement(filter: ServiceRequestsFilter):
+    async with async_session() as session:
+        request_repo = ServiceRequestRepository(session)
+        await measure_query(query_to_measure_1, request_repo, filter, num_runs=measurements_number)
+
+async def main():
+    my_filter_1 = ServiceRequestsFilter(lineid=30)
+    my_filter_2 = ServiceRequestsFilter(SortByDate=True)
+    my_filter_3 = ServiceRequestsFilter(userId=1, SortByDate=True, status="открыта")
+
+    await asyncio.gather(
+        run_measurement(my_filter_1),
+        run_measurement(my_filter_2),
+        run_measurement(my_filter_3),
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- src/app/session.py
+++ src/app/session.py
@@ -1,19 +1,22 @@
 import sqlalchemy
 import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from config import settings
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from app.models.models import *
+from app.config import settings
+from sqlalchemy.orm import DeclarativeBase
+from app.models.models import Base
+
+
+engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+    pool_pre_ping=True
+)
+
+async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def create_session(recreate=False):
     print("Версия SQL Alchemy:", sqlalchemy.__version__)
 
-    engine = create_async_engine(
-        url=settings.DATABASE_URL_asyncpg,
-        echo=False,
-        pool_pre_ping=True
-    )
-    
     try:
         async with engine.connect() as conn:
             print("DB successfully connected")
@@ -21,22 +24,17 @@ async def create_session(recreate=False):
         print(f"Connection to db failed: {e}")
         return None
 
-    async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-    db = async_session()
-
     if recreate:
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.drop_all)
             await conn.run_sync(Base.metadata.create_all)
-    
-    return db
+
+    # Возвращаем объект фабрики сессий
+    return async_session
 
 async def get_db():
-    db = await create_session()
-    try:
-        yield db
-    finally:
-        await db.close()
+    async with async_session() as session:
+        yield session
 
 if __name__ == "__main__":
     asyncio.run(create_session(True))
