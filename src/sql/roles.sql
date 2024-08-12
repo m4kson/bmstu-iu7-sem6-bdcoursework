b601efbd08f0397ebae9cfe91c3a1116c76089ed
m4kson <limos.00@mail.ru>
2024-08-12 16:09:12 +0300

    sql roles script fixed

--- src/sql/roles.sql
+++ src/sql/roles.sql
@@ -1,21 +1,77 @@
 -- Создание ролей (пользователей) с паролями
-CREATE ROLE operator_production WITH SUPERUSER CREATEDB CREATEROLE CONNECTION LIMIT -1 LOGIN PASSWORD 'operator';
-CREATE ROLE service_specialist WITH NOSUPERUSER NOCREATEDB NOCREATEROLE CONNECTION LIMIT -1 LOGIN PASSWORD 'specialist';
-CREATE ROLE administrator WITH NOSUPERUSER NOCREATEDB NOCREATEROLE CONNECTION LIMIT -1 LOGIN PASSWORD 'admin';
-CREATE ROLE unauthorized_user WITH NOSUPERUSER NOCREATEDB NOCREATEROLE CONNECTION LIMIT -1;
+CREATE ROLE operator_production WITH 
+NOSUPERUSER 
+NOCREATEDB 
+NOCREATEROLE 
+CONNECTION LIMIT -1 
+LOGIN 
+PASSWORD 'operator';
+
+CREATE ROLE service_specialist WITH 
+NOSUPERUSER 
+NOCREATEDB 
+NOCREATEROLE 
+CONNECTION LIMIT -1 
+LOGIN 
+PASSWORD 'specialist';
+
+CREATE ROLE administrator WITH 
+SUPERUSER 
+CREATEDB 
+CREATEROLE 
+CONNECTION LIMIT -1 
+LOGIN 
+PASSWORD 'admin';
+
+CREATE ROLE unauthorized_user WITH
+NOSUPERUSER 
+NOCREATEDB
+NOCREATEROLE 
+CONNECTION LIMIT -1;
 
 -- Назначение прав ролям
-GRANT SELECT ON Tractors, AssemblyLines, ServiceReports, ServiceRequests TO operator_production;
-GRANT UPDATE ON AssemblyLines, ServiceRequests, Users TO operator_production;
-GRANT INSERT ON ServiceRequests TO operator_production;
+GRANT SELECT ON 
+tractors, 
+assemblylines, 
+servicereports, 
+servicerequests, 
+tractor_line 
+TO operator_production;
 
-GRANT SELECT ON Tractors, AssemblyLines, ServiceRequests, ServiceReports, Details, DetailOrders TO service_specialist;
-GRANT UPDATE ON DetailOrders, ServiceReports, Users TO service_specialist;
-GRANT INSERT ON ServiceReports, DetailOrders TO service_specialist;
+GRANT UPDATE ON 
+assemblylines, 
+servicerequests
+TO operator_production;
 
--- (*) Здесь следует уточнить, какие права должны быть у администратора
--- Предположим, что администратору должны быть предоставлены все возможные права на все таблицы
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO administrator;
+GRANT INSERT ON 
+servicerequests
+TO operator_production;
 
--- Права для неавторизованных пользователей
-REVOKE ALL ON Users FROM public; -- Убираем все права на таблицу Users
+GRANT SELECT ON 
+tractors, 
+assemblylines, 
+servicerequests, 
+servicereports, 
+details, 
+detailorders, 
+order_detail,
+tractor_line,
+line_detail
+TO service_specialist;
+
+GRANT UPDATE ON 
+detailorders, 
+servicereports, 
+order_detail
+TO service_specialist;
+
+GRANT INSERT ON 
+servicereports, 
+detailorders,
+order_detail
+TO service_specialist;
+
+GRANT ALL PRIVILEGES ON 
+ALL TABLES 
+IN SCHEMA public 
+TO administrator;
